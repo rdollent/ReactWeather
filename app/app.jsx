d5195eb5880ca170f48f2dcb9093e28a4e349b0c
rdollent <rumbalumba89@gmail.com>
2018-07-26 22:11:45 -0600

    added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -9,6 +9,11 @@ var Weather = require("Weather");
 var About = require("About");
 var Examples = require("Examples");
 
+// load foundation
+require("style!css!foundation-sites/dist/foundation.min.css");
+// require does not know how to load css. use css! loader
+$(document).foundation();
+
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
